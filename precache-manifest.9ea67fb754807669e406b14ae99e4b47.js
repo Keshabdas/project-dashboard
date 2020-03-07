a737164f713c13f7fe4b12f1269ffe8c3e32a4eb
Keshabdas <keshabad356@gmail.com>
2020-03-07 20:10:31 +0530

    Updates

---
+++ precache-manifest.9ea67fb754807669e406b14ae99e4b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3dce235d7827a056b8de453e776c2d",
+    "url": "/project-dashboard/index.html"
+  },
+  {
+    "revision": "ac8f195bdda0d35e2d30",
+    "url": "/project-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b115776872ec5580fac9",
+    "url": "/project-dashboard/static/js/2.7258dbec.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/project-dashboard/static/js/2.7258dbec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8f195bdda0d35e2d30",
+    "url": "/project-dashboard/static/js/main.b3395dc6.chunk.js"
+  },
+  {
+    "revision": "199bb7fb446a1e2af96e",
+    "url": "/project-dashboard/static/js/runtime-main.cb938f85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
